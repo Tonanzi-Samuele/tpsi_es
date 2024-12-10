50a712b4b444d3240c6ef6bacef77717a08991ab
Tonanzi-Samuele <samuele.github@gmail.com>
2024-12-10 12:32:19 +0100

    added changes

---
+++ JS/es_12/tonanzi_esercizio_js_12.js
@@ -0,0 +1,9 @@
+let Animale = function (razza, nome, colore){
+    (this.razza = razza)
+    (this.nome = nome)
+    (this.colore = colore)
+    (this.speak = function (){
+        console.log("verso")
+    });
+    (this.)
+}
